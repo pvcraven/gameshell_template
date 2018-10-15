edbe1466957c3f9f15148396595da46bdaf844a2
Paul Vincent Craven <paul@cravenfamily.com>
2018-10-14 19:59:27 -0500

    Copy in shooting game

--- src/my_arcade_game.py
+++ src/my_arcade_game.py
@@ -1,38 +1,178 @@
+"""
+Sprite Bullets
+
+Simple program to show basic sprite usage.
+
+Artwork from http://kenney.nl
+
+If Python and Arcade are installed, this example can be run from the command line with:
+python -m arcade.examples.sprite_bullets
+"""
+import random
 import arcade
+import os
+
+SPRITE_SCALING_PLAYER = 0.25
+SPRITE_SCALING_COIN = 0.1
+SPRITE_SCALING_LASER = 0.3
+COIN_COUNT = 50
 
 SCREEN_WIDTH = 320
 SCREEN_HEIGHT = 240
 
+BULLET_SPEED = 5
+MOVEMENT_SPEED = 2
+
+class Bullet(arcade.Sprite):
+    def update(self):
+        self.center_y += BULLET_SPEED
+
 
 class MyGame(arcade.Window):
-    """
-    Main application class.
-    """
+    """ Main application class. """
 
     def __init__(self):
-        super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT, fullscreen=True)
+        """ Initializer """
+        # Call the parent class initializer
+        super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT, "Sprites and Bullets Demo")
 
-        arcade.set_background_color(arcade.color.WHITE)
+        # Set the working directory (where we expect to find files) to the same
+        # directory this .py file is in. You can leave this out of your own
+        # code, but it is needed to easily run the examples using "python -m"
+        # as mentioned at the top of this program.
+        file_path = os.path.dirname(os.path.abspath(__file__))
+        os.chdir(file_path)
+
+        # Variables that will hold sprite lists
+        self.player_list = None
+        self.coin_list = None
+        self.bullet_list = None
+
+        # Set up the player info
+        self.player_sprite = None
+        self.score = 0
+
+        # Load sounds. Sounds from kenney.nl
+        self.gun_sound = arcade.sound.load_sound("sounds/laser1.wav")
+        self.hit_sound = arcade.sound.load_sound("sounds/phaseJump1.wav")
+
+        arcade.set_background_color(arcade.color.AMAZON)
 
     def setup(self):
-        """ Set up the game here. Call this function to restart the game. """
-        pass
+
+        """ Set up the game and initialize the variables. """
+
+        # Sprite lists
+        self.player_list = arcade.SpriteList()
+        self.coin_list = arcade.SpriteList()
+        self.bullet_list = arcade.SpriteList()
+
+        # Set up the player
+        self.score = 0
+
+        # Image from kenney.nl
+        self.player_sprite = arcade.Sprite("images/character.png", SPRITE_SCALING_PLAYER)
+        self.player_sprite.center_x = 50
+        self.player_sprite.center_y = 15
+        self.player_list.append(self.player_sprite)
+
+        # Create the coins
+        for i in range(COIN_COUNT):
+
+            # Create the coin instance
+            # Coin image from kenney.nl
+            coin = arcade.Sprite("images/coin_01.png", SPRITE_SCALING_COIN)
+
+            # Position the coin
+            coin.center_x = random.randrange(SCREEN_WIDTH)
+            coin.center_y = random.randrange(50, SCREEN_HEIGHT)
+
+            # Add the coin to the lists
+            self.coin_list.append(coin)
+
+        # Set the background color
+        arcade.set_background_color(arcade.color.AMAZON)
 
     def on_draw(self):
         """
         Render the screen.
         """
 
+        # This command has to happen before we start drawing
         arcade.start_render()
-        arcade.draw_circle_filled(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, 50, arcade.color.RED)
-        arcade.draw_circle_filled(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, 50, arcade.color.BLUE)
 
-    def on_key_press(self, symbol: int, modifiers: int):
-        exit()
+        # Draw all the sprites.
+        self.coin_list.draw()
+        self.bullet_list.draw()
+        self.player_list.draw()
+
+        # Render the text
+        arcade.draw_text(f"Score: {self.score}", 10, SCREEN_HEIGHT - 12, arcade.color.WHITE, 12)
+
+    def on_key_press(self, key, modifiers):
+        """Called whenever a key is pressed. """
+
+        if key == arcade.key.LEFT:
+            self.player_sprite.change_x = -MOVEMENT_SPEED
+        elif key == arcade.key.RIGHT:
+            self.player_sprite.change_x = MOVEMENT_SPEED
+        elif key == arcade.key.M:
+            # Gunshot sound
+            arcade.sound.play_sound(self.gun_sound)
+            # Create a bullet
+            bullet = Bullet("images/laserBlue01.png", SPRITE_SCALING_LASER)
+
+            # The image points to the right, and we want it to point up. So
+            # rotate it.
+            bullet.angle = 90
+
+            # Position the bullet
+            bullet.center_x = self.player_sprite.center_x
+            bullet.bottom = self.player_sprite.top
+
+            # Add the bullet to the appropriate lists
+            self.bullet_list.append(bullet)
+
+    def on_key_release(self, key, modifiers):
+        """Called when the user releases a key. """
+
+        if key == arcade.key.UP or key == arcade.key.DOWN:
+            self.player_sprite.change_y = 0
+        elif key == arcade.key.LEFT or key == arcade.key.RIGHT:
+            self.player_sprite.change_x = 0
+
+    def update(self, delta_time):
+        """ Movement and game logic """
+
+        # Call update on bullet sprites
+        self.bullet_list.update()
+        self.player_list.update()
+
+        # Loop through each bullet
+        for bullet in self.bullet_list:
+
+            # Check this bullet to see if it hit a coin
+            hit_list = arcade.check_for_collision_with_list(bullet,
+                                                            self.coin_list)
+
+            # If it did, get rid of the bullet
+            if len(hit_list) > 0:
+                bullet.kill()
+
+            # For every coin we hit, add to the score and remove the coin
+            for coin in hit_list:
+                coin.kill()
+                self.score += 1
+
+                # Hit Sound
+                arcade.sound.play_sound(self.hit_sound)
+
+            # If the bullet flies off-screen, remove it.
+            if bullet.bottom > SCREEN_HEIGHT:
+                bullet.kill()
 
 
 def main():
-    """ Main method """
     window = MyGame()
     window.setup()
     arcade.run()
